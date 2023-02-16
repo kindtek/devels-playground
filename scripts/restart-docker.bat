a69d3ab1b9c2db750e06bb335b838f989406908f
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-16 03:28:45 -0800

    debug tool added

--- scripts/restart-docker.bat
+++ scripts/restart-docker.bat
@@ -2,13 +2,28 @@
 @REM ..if docker hangs when it tries to start
 @REM also try deleting the .docker folder in user directory (C:\Users\xxxx)
 
+SET prompt=prompt
+:prompt
+
+SET /p "prompt=[u]nregister docker desktop containers, [s]tart docker service (admin req), [r]eset wsl default distro, or [reboot] windows ?"
+goto %prompt%
+
+:u
 @REM force restart for docker containers
 wsl --unregister docker-desktop
 wsl --unregister docker-desktop-data
 "C:\Program Files\Docker\Docker\DockerCli.exe" -SwitchDaemon
+goto prompt
 
-@REM switch to wsl distro named "Ubuntu"
-@REM wsl -s Ubuntu 
+:s
+"C:\Windows\System32\net.exe" start "com.docker.service"
+goto prompt
 
+@REM reset default wsl distro
+:r
+wsl -s Ubuntu-20.04
+goto prompt
+
+:reboot
 @REM restart windows
 @REM shutdown -r -t 0
