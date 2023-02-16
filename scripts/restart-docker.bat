768bebd87ac931daba91e701fa9f5746eb8d404e
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-16 03:59:46 -0800

    cleanup menu prompt

--- scripts/restart-docker.bat
+++ scripts/restart-docker.bat
@@ -1,14 +1,24 @@
+@echo off
+SETLOCAL EnableDelayedExpansion
+
 @REM this is useful for when docker refuses to start due to a bad wsl distro set as default or ...
 @REM ..if docker hangs when it tries to start
 @REM also try deleting the .docker folder in user directory (C:\Users\xxxx)
 
 SET prompt=prompt
 :prompt
+ECHO:
+ECHO  [s]soft docker restart (admin req)
+ECHO  [u]nregister docker desktop containers
+ECHO  [h]ard docker service start (admin req)
+ECHO  [r]eset wsl default distro
+ECHO  [reboot] windows (^^!^^!)
+ECHO   [q]uit
 
-SET /p "prompt=[u]nregister docker desktop containers, [s]tart docker service (admin req), [r]eset wsl default distro, or [reboot] windows ?"
-goto %prompt%
+SET /p "prompt=$ "
+goto !prompt!
 
-:d
+:s
 net stop docker
 net stop com.docker.service
 taskkill /IM "dockerd.exe" /F
@@ -25,7 +35,7 @@ wsl --unregister docker-desktop-data
 "C:\Program Files\Docker\Docker\DockerCli.exe" -SwitchDaemon
 goto prompt
 
-:s
+:h
 "C:\Windows\System32\net.exe" start "com.docker.service"
 goto prompt
 
@@ -37,3 +47,9 @@ goto prompt
 :reboot
 @REM restart windows
 @REM shutdown -r -t 0
+
+:q
+:quit
+:exit
+:end
+:x
