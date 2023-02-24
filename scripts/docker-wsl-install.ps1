791a3c69a56f44de0c73e50c643f9a99a0c5a2cc
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-24 13:21:52 -0800

    fix typo and bugs

--- scripts/docker-wsl-install.ps1
+++ scripts/docker-wsl-install.ps1
@@ -147,15 +147,15 @@ if ($principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
         # get a head start on building custom docker images using machine settings
         $pwd_path = Split-Path -Path $PSCommandPath
         $full_path = "& $pwd_path/docker-to-wsl/scripts/images-build.bat" 
-        Start-Process "cmd.exe" "/c $full_path" runAs -Wait -WindowStyle "Minimized"
+        Start-Process "cmd.exe" "/c $full_path" -Verb runAs -Wait -WindowStyle "Minimized"
 
         # start WSL docker import tool
         $pwd_path = Split-Path -Path $PSCommandPath
         $full_path = "& $pwd_path/docker-to-wsl/scripts/wsl-import.ps1" 
         # Start-Process "powershell" "/c $full_path"
-        Start-Process powershell.exe -command $full_path -Verb runAs -Wait -WindowStyle "Maximized"
+        Start-Process powershell.exe $full_path -Verb runAs -Wait -WindowStyle "Maximized"
 
-        $user_input = (Read-Host "`r`nopen Docker Dev enviornment? [y]/n")
+        $user_input = (Read-Host "`r`nopen Docker Dev environment? [y]/n")
         if ( $user_input -ne 'n' ) {
             Start-Process "https://open.docker.com/dashboard/dev-envs?url=https://github.com/kindtek/docker-to-wsl@dev" -Wait -WindowStyle "Hidden"
         }  
