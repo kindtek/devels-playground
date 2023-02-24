3cc7b00d1f18910db48fd99245bfa9f92c00c800
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-24 11:14:07 -0800

    color fix

--- scripts/docker-wsl-install.ps1
+++ scripts/docker-wsl-install.ps1
@@ -1,7 +1,7 @@
 # if (Split-Path -Parent){
 $PSCommandPath | Split-Path -Parent
 Set-ConsoleColor -foreground White -background Black -clear
-$host.ui.rawui.BackgroundColor = "Black"
+
 function install_software {
     param (
         $software_id,
