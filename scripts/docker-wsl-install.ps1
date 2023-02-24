f2fb48d6580dd817ef1c30c25468c9a04f38bcd5
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-24 01:21:29 -0800

    add install functions and scripts

--- scripts/docker-wsl-install.ps1
+++ scripts/docker-wsl-install.ps1
@@ -1,7 +1,137 @@
-Start-Process "https://www.microsoft.com/p/app-installer/9nblggh4nns1"
-winget install -e --id GitHub.cli
-winget install Microsoft.VisualStudioCode --override '/SILENT /mergetasks="!runcode,addcontextmenufiles,addcontextmenufolders"' 
-winget install --id=Docker.DockerDesktop  -e
+$PSCommandPath | Split-Path -Parent
+
+function install_software {
+    param (
+        $software_id,
+        $software_name,
+        $install_command,
+        $verify_installed,
+        $force_install
+    )
+    # function built using https://keestalkstech.com/2017/10/powershell-snippet-check-if-software-is-installed/
+
+    if ($verify_installed -eq $true) {
+        $installed = (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where { $_.DisplayName -eq $software_id }) -ne $null
+        if (-Not $installed) {
+            if ($force_install -ne $true) {
+                # give user the option to install
+                if ($install -eq 'y' -Or $install -eq 'yes') { 
+                    $install = Read-Host "$software_name required but not found. Install now? (y/[n])".ToLower()
+                    Write-Host "Installing $software_name..."
+                    Invoke-Expression $install_command
+                }
+                else {
+                    Write-Host "skipping $software_name install"
+                }
+                
+            }
+            else {
+                Write-Host "Installing $software_name..."
+                Invoke-Expression $install_command
+            }
+        }
+        else {
+            Write-Host "$software_name already installed."
+        }
+    }
+    else {
+        $install = Read-Host "$software_name required. Install now? (y/[n])"
+        $install = $install.ToLower()
+        if ($install -eq 'y' -Or $install -eq 'yes') { 
+            Write-Host "Installing $software..."
+            Invoke-Expression $install_command
+        }
+        else {
+            Write-Host "skipping $software_name install"
+        }
+    }
+    
+}
+
+
+# prompt to install winget
+
+$principal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+if ($principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+
+
+    # use windows-features-wsl-add to handle windows features install
+
+    # $goto_path = Join-Path -Path (Get-Location) -Childpath "windows-features-wsl-add/configure-windows-features.ps1"
+    $pwd_path = Split-Path -Path $PSCommandPath
+    write-host "-pwd_path: " + $pwd_path
+    $full_path = Join-Path -Path $pwd_path -ChildPath "/windows-features-wsl-add/configure-windows-features.ps1"
+    write-host "full path: $full_path"
+    powershell "$full_path"
+
+    # # software_id and software_name equal since installation $verify_installed set to false
+    # $software_id = $software_name = "Windows Subsystem for Linux (WSL)"
+    # $install_command = "Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart"
+    # $verify_installed = $false
+    # $force_install = $true
+    # install_software $software_id $software_name $install_command $verify_installed $force_install
+
+    # $software_id = $software_name = "Hyper-V"
+    # $install_command = "Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart"
+    # $verify_installed = $false
+    # $force_install = $true
+    # install_software $software_id $software_name $install_command $verify_installed $force_install
+
+    # $software_id = $software_name = "Powershell 2.0"
+    # $install_command = "Enable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2 -All -NoRestart"
+    # $verify_installed = $false
+    # $force_install = $false
+    # install_software $software_id $software_name $install_command $verify_installed $force_install
+
+    $software_id = $software_name = "winget"
+    $install_command = "powershell.exe -ExecutionPolicy Unrestricted -command $pwd_path/get-latest-winget.ps1"
+    $verify_installed = $false
+    $force_install = $true
+    
+    install_software $software_id $software_name $install_command $verify_installed $force_install
+        
+    # @TODO: find a way to check if windows terminal is installed
+    $software_id = $software_name = "Windows Terminal"
+    $install_command = "winget install Microsoft.WindowsTerminal"
+    $verify_installed = $false
+    $force_install = $false
+    install_software $software_id $software_name $install_command $verify_installed $force_install
+
+    $software_id = "Git_is1"
+    $software_name = "Github CLI"
+    $install_command = "winget install -e --id GitHub.cli"
+    $verify_installed = $true
+    $force_install = $true
+    install_software $software_id $software_name $install_command $verify_installed $force_install
+
+    # @TODO: find a way to check if VSCode is installed
+    $software_id = $software_name = "Visual Studio Code (VSCode)"
+    $install_command = "winget install Microsoft.VisualStudioCode --override '/SILENT /mergetasks=`"!runcode,addcontextmenufiles,addcontextmenufolders`"'"
+    $verify_installed = $false
+    $force_install = $true
+    install_software $software_id $software_name $install_command $verify_installed $force_install
+
+
+    # Docker Desktop happens to work for both id and name
+    $software_id = $software_name = "Docker Desktop"
+    $verify_installed = $true
+    $force_install = $true
+    install_command = "winget install --id=Docker.DockerDesktop  -e"
+
+    $confirmation = Read-Host "A restart is required for the changes to take effect. Restart now (y/n)?"
+    if ($confirmation -ieq 'y') {
+        Restart-Computer -Force
+    }
+    else {
+        Write-Host "At a time convenient to you, restart the machine for the changes to take effect."
+    }
+
+}
+else {
+    Start-Process -FilePath "powershell" -ArgumentList "$('-File ""')$(Get-Location)$('\')$($MyInvocation.MyCommand.Name)$('""')" -Verb runAs
+}    
+
+
 Start-Process "https://open.docker.com/dashboard/dev-envs?url=https://github.com/kindtek/docker-to-wsl@dev"
 # ./images-build-push.ps1
 ./wsl-import.ps1
