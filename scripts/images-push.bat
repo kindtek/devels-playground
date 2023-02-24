5e2b5671c7b1bb99574fd55550b7b08bb58db6db
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-24 04:19:38 -0800

    split images-build-push into images-build and images-push

---
+++ scripts/images-push.bat
@@ -0,0 +1,18 @@
+@REM TODO: build images with custom username/groupname then push
+@REM docker build --build-arg username=!username! --build-arg groupname=!groupname! -f ../dockerfile.alpine.yaml -t d2w_phat
+@REM docker build --build-arg username=!username! --build-arg groupname=!groupname! -f ../dockerfile.ubuntu.yaml -t d2w_phat
+@REM docker image push kindtek/d2w
+
+@REM ubuntu
+docker compose -f ../docker-compose.ubuntu.yaml up -d
+@REM alpine
+docker compose -f ../docker-compose.alpine.yaml up -d
+
+@REM ubuntu
+docker compose -f ../docker-compose.ubuntu.yaml push
+@REM alpine
+docker compose -f ../docker-compose.alpine.yaml push
+
+@REM both
+@REM docker compose -f ../docker-compose.ubuntu.yaml -f ../docker-compose.alpine.yaml up -d
+
