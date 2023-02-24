40643b5e73e32ac4217c308548c39b2f2b63b560
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-24 15:46:29 -0800

    debug

--- scripts/docker-wsl-install.ps1
+++ scripts/docker-wsl-install.ps1
@@ -82,16 +82,18 @@ function restart_prompt {
     }
 }
 
-# open terminal with admin priveleges
-$principal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+
 
 $pwd_path = Split-Path -Path $PSCommandPath
 # get a head start on building custom docker images using machine settings and without admin priveleges
 $pwd_path = Split-Path -Path $PSCommandPath
 $full_path = "& $pwd_path/docker-to-wsl/scripts/images-build.bat" 
 $cmd_args = 'cmd.exe' + ' ' + $full_path
+Write-Host  "cmd args: $cmd_args"
 Invoke-WmiMethod -Class Win32_Process -Name Create -ArgumentList $cmd_args
 
+# open terminal with admin priveleges
+$principal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 if ($principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
 
     $software_id = $software_name = "WinGet"
