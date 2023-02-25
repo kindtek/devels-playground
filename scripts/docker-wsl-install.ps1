ffe3cb0e1a728e7577d250c09548492a71ceeaa8
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-24 21:31:34 -0800

    housekeeping

--- scripts/docker-wsl-install.ps1
+++ scripts/docker-wsl-install.ps1
@@ -84,15 +84,13 @@ function restart_prompt {
 
 
 
-$pwd_path = Split-Path -Path $PSCommandPath
 # get a head start on building custom docker images using machine settings and without admin priveleges
 $pwd_path = Split-Path -Path $PSCommandPath
-# holy moly this was tough - great reference here: https://slai.github.io/posts/powershell-and-external-commands-done-right/
+Push-Location 'docker-to-wsl/scripts'
+# holy moly getting this invoke-wmimethod was tough - great reference here: https://slai.github.io/posts/powershell-and-external-commands-done-right/
 $cmd_args = "$pwd_path/docker-to-wsl/scripts/images-build.bat" 
 &$cmd_args = Invoke-WmiMethod -Class Win32_Process -Name Create -ArgumentList cmd "$pwd_path/docker-to-wsl/scripts/images-build.bat"
-# $cmd_args = "cmd.exe `"$full_path`""
-Write-Host  "cmd ardgs: $cmd_args"
-
+Pop-Location
 
 # open terminal with admin priveleges
 $principal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
