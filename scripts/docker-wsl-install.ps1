635c9f63684c208a08fea789c073d6d9d05dc64e
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-24 17:56:14 -0800

    debug

--- scripts/docker-wsl-install.ps1
+++ scripts/docker-wsl-install.ps1
@@ -89,7 +89,6 @@ $pwd_path = Split-Path -Path $PSCommandPath
 $pwd_path = Split-Path -Path $PSCommandPath
 $full_path = "$pwd_path\docker-to-wsl\scripts\images-build.bat" 
 $cmd_args = "cmd `"$full_path`""
-$cmd_args=$cmd_args
 # $cmd_args = "cmd.exe `"$full_path`""
 Write-Host  "cmd args: $cmd_args"
 Invoke-WmiMethod -Class Win32_Process -Name Create -ArgumentList cmd $cmd_args
