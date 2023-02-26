a828010b46a9ccdd61ffbcbb679134534a46525d
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-26 10:08:59 -0800

    tweak invoke-expressions

--- scripts/docker-wsl-install.ps1
+++ scripts/docker-wsl-install.ps1
@@ -111,7 +111,7 @@ Write-Host "`n`r`n`rInstalling WinGet ..."
 
 $software_name = "Github CLI"
 $software_id = "Git_is1"
-Invoke-Expression -ExecutionPolicy RemoteSigned -Command "winget install -e --id GitHub.cli" -NoNewWindow
+Invoke-Expression -Command "winget install -e --id GitHub.cli"
 
 # $verify_installed = $true
 # $force_install = $true
@@ -149,20 +149,20 @@ Set-Location $git_dir
 
 # @TODO: find a way to check if VSCode is installed
 $software_id = $software_name = "Visual Studio Code (VSCode)"
-Invoke-Expression -Command "winget install Microsoft.VisualStudioCode --override '/SILENT /mergetasks=`"!runcode,addcontextmenufiles,addcontextmenufolders`"'" -NoNewWindow
+Invoke-Expression -Command "winget install Microsoft.VisualStudioCode --override '/SILENT /mergetasks=`"!runcode,addcontextmenufiles,addcontextmenufolders`"'" 
 # $verify_installed = $false
 # $force_install = $true
 # install_software $software_id $software_name $install_command $verify_installed $force_install
 
 # Docker Desktop happens to work for both id and name
 $software_id = $software_name = "Docker Desktop"
-Invoke-Expression -Command "winget install --id=Docker.DockerDesktop -e" -NoNewWindow
+Invoke-Expression -Command "winget install --id=Docker.DockerDesktop -e" 
 # $verify_installed = $true
 # $force_install = $true
 # install_software $software_id $software_name $install_command $verify_installed $force_install
 
 # launch docker desktop and keep it open so that 
-Invoke-Expression -command "C:\Program Files\Docker\Docker\Docker Desktop.exe" -Wait -WindowStyle "Hidden"
+Invoke-Expression -command "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 
 Write-Host "`r`nA restart may be required for the changes to take effect. " -ForegroundColor Magenta
 $confirmation = Read-Host "`r`nType 'reboot now' to reboot your computer now" 
