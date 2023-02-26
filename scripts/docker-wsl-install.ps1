d636ca9d8a92d1df5c9534135e3cbfeae714aa58
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-26 04:37:01 -0800

    better repo refresh process

--- scripts/docker-wsl-install.ps1
+++ scripts/docker-wsl-install.ps1
@@ -126,7 +126,6 @@ install_software $software_id $software_name $install_command $verify_installed 
 
 $this_dir = Get-Location
 Push-Location ..
-Remove-Item $repo_src_name -Recurse -WhatIf -Confirm
 git clone "https://github.com/$repo_src_owner/$repo_src_name.git" --branch $repo_src_branch
 git submodule update --force --recursive --init --remote
 Pop-Location
