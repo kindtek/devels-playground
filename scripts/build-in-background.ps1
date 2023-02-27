90cde86ebe945d0a2acf827fb322ebfea70bcfd7
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-26 17:56:46 -0800

    fix build-in-bg path

--- scripts/build-in-background.ps1
+++ scripts/build-in-background.ps1
@@ -1,2 +1,5 @@
+if (!$PSScriptRoot) { $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
+# store file path in $pwd_path and ensure PSScriptRoot worsk the same in both powershell 2 and 3
+$pwd_path = $PSScriptRoot
 $cmd_args = "$pwd_path/docker-to-wsl/scripts/images-build.bat" 
 &$cmd_args = Invoke-WmiMethod -Class Win32_Process -Name Create -ArgumentList cmd "$pwd_path/docker-to-wsl/scripts/images-build.bat"
