2aacedbb2752d59477616059ed9c7e403c2333f5
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-26 18:22:15 -0800

    fix pathname

--- scripts/build-in-background.ps1
+++ scripts/build-in-background.ps1
@@ -3,5 +3,5 @@ $host.UI.RawUI.BackgroundColor = "Black"
 if (!$PSScriptRoot) { $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
 # store file path in $pwd_path and ensure PSScriptRoot worsk the same in both powershell 2 and 3
 $pwd_path = $PSScriptRoot
-$cmd_args = "$pwd_path/docker-to-wsl/scripts/images-build.bat" 
+$cmd_args = "$pwd_path/images-build.bat" 
 &$cmd_args = Invoke-WmiMethod -Class Win32_Process -Name Create -ArgumentList cmd "$pwd_path/images-build.bat"
