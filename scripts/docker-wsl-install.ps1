2809692f40baa079863281b84fcbf2551f720bfe
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-28 01:02:22 -0800

    try to keep going with the manually downloaded files

--- scripts/docker-wsl-install.ps1
+++ scripts/docker-wsl-install.ps1
@@ -60,21 +60,26 @@ $git_dir += "/$repo_src_name"
 
 Set-Location ../../
 
-try {
+    # refresh environment variables
     # refresh environment variables
     RefreshEnv
     # test git
     $git_version = git --version 
     
     # if it works remove the directory and the manually downloaded files..
-    if (Test-Path -Path "$git_dir") {
-        Remove-Item "$git_dir" -Force -Recurse -ErrorAction SilentlyContinue
-    }
+    RefreshEnv
+    # test git
+    $git_version = git --version 
     $host.UI.RawUI.BackgroundColor = "Black"
     # .. and then clone the repo
     git clone "https://github.com/$repo_src_owner/$repo_src_name.git" --branch $repo_src_branch "$git_dir"
     Set-Location "$git_dir"
     $host.UI.RawUI.BackgroundColor = "Black"
+    git submodule update --force --recursive --init --remote
+    $host.UI.RawUI.BackgroundColor = "Black"
+}
+# otherwise try to keep going with the manually downloaded files
+catch {}
     git submodule update --force --recursive --init --remote
     $host.UI.RawUI.BackgroundColor = "Black"
 }
