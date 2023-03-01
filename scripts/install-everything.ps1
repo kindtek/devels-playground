16430ab7b2f72a5e900067fd40514ed3b9afdc7c
kindtek <98300737+kindtek@users.noreply.github.com>
2023-03-01 07:36:22 -0800

    fix quote fr fr

--- scripts/install-everything.ps1
+++ scripts/install-everything.ps1
@@ -200,10 +200,8 @@ try {
         }
         else {
             Write-Host "Failed to launch docker. Not able to start Devel's Playground. Please restart and run the script again:" -ForegroundColor "Yellow"
-            Write-Host "powershell -executionpolicy remotesigned -Command `"Invoke-WebRequest https://raw.githubusercontent.com/kindtek/powerhell-remote/devels-workshop/install.ps1 -OutFile install-kindtek-devels-workshop.ps1; powershell -executionpolicy remotesigned -File install-kindtek-devels-workshop.ps1`""
+            Write-Host "powershell -executionpolicy remotesigned -Command `"Invoke-WebRequest https://raw.githubusercontent.com/kindtek/powerhell-remote/devels-workshop/install.ps1 -OutFile install-kindtek-devels-workshop.ps1`"; powershell -executionpolicy remotesigned -File install-kindtek-devels-workshop.ps1"
         }
-
-
     }
 }
 catch {}
