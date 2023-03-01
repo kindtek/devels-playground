7443a513be2e032b2ddfb5a6b976a2e05d402904
kindtek <98300737+kindtek@users.noreply.github.com>
2023-03-01 11:18:04 -0800

    finally got git_path, parent_path to work

--- scripts/install-everything.ps1
+++ scripts/install-everything.ps1
@@ -9,7 +9,7 @@ $repo_src_branch = 'windows'
 $git_path = $pwd_path.Replace("\scripts", "")
 $git_path = $git_path.Replace("/scripts", "")
 $parent_path = $git_path.Replace("\$repo_src_name", "")
-$parent_path = $parent_path.ReReplacemove("/$repo_src_name", "")
+$parent_path = $parent_path.Remove("/$repo_src_name", "")
 Write-Host "parent path: $parent_path"
 Write-Host "git dir: $git_path"
 Write-Host "scripts dir: $pwd_path"
