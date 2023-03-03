f85f41898d1da269aeb77f74e50df68714a40cbb
kindtek <98300737+kindtek@users.noreply.github.com>
2023-03-03 15:18:02 -0800

    return -1 index value

--- scripts/wsl-docker-import.ps1
+++ scripts/wsl-docker-import.ps1
@@ -165,8 +165,8 @@ function dev_boilerplate {
     Write-Host "$install_location`r`n"
 
     docker_image_pull $image_repo_image_name
-    $WSL_DOCKER_CONTAINER_ID_RAW = @(docker_container_start $config $distro $image_repo_image_name $install_location)
-    $WSL_DOCKER_CONTAINER_ID = $WSL_DOCKER_CONTAINER_ID_RAW[0]
+    $WSL_DOCKER_CONTAINER_ID = (docker_container_start $config $distro $image_repo_image_name $install_location)[-1]
+    # $WSL_DOCKER_CONTAINER_ID = $WSL_DOCKER_CONTAINER_ID_RAW[0]
 
     Write-Host "WSL_DOCKER_CONTAINER_ID=$WSL_DOCKER_CONTAINER_ID"
     Write-Host "before-install_location=$install_location"
