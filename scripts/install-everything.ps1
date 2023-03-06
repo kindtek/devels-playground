50c2e099c95e939d6e1f3d8df462f354e8e55107
kindtek <98300737+kindtek@users.noreply.github.com>
2023-03-05 20:36:35 -0800

    fix conditional statement

--- scripts/install-everything.ps1
+++ scripts/install-everything.ps1
@@ -285,8 +285,7 @@ function run_devels_playground {
         $software_name = "Docker Desktop"
         # if ($start_devs_playground -ine 'n' -And $start_devs_playground -ine 'no') { 
         Write-Host "`r`nNOTE: $software_name is required to be running for the Devel's Playground to work.`r`nDo NOT quit $software_name until you are done running it.`r`n" 
-        $docker_online = require_docker_online
-        if ($docker_online -eq $true) {
+        if (require_docker_online) {
             # // commenting out background building process because this is NOT quite ready.
             # // would like to run in separate window and then use these new images in devel's playground 
             # // if they are more up to date than the hub - which could be a difficult process
