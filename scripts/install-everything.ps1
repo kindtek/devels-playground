f0b46b5b7e01d8ccf0205c81806e047d90659741
kindtek <98300737+kindtek@users.noreply.github.com>
2023-03-05 21:31:09 -0800

    remove try catch for debug

--- scripts/install-everything.ps1
+++ scripts/install-everything.ps1
@@ -140,7 +140,7 @@ function install_repo {
     param (
         $parent_path, $git_path, $repo_src_owner, $repo_src_name, $repo_src_branch 
     )
-    try {
+    # try {
         Write-Host "Now installing:`r`n`t- Chocolatey`r`n`t- Python`r`n" 
 
         # refresh environment variables using script in choco temp download location
@@ -219,7 +219,7 @@ function install_repo {
     
     }
     # if git is not recognized try to limp along with the manually downloaded files
-    catch {}
+    # catch {}
 }
 
 # refresh env again
