17fd886fcce79f6eef88d4884481ea13cf01154b
kindtek <n8@kindtek.io>
2023-03-19 13:38:26 -0700

    syntax correction

--- scripts/gh-auth-first-time-login.sh
+++ scripts/gh-auth-first-time-login.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
 git_user_email=$GH_REPO_OWNER_EMAIL
 git_user_name=kindtek
-ssh_dir=/home/${1:-dvl}/.ssh
+ssh_dir=/home/$(echo ${1:-dvl})/.ssh
+if ! [ -d $ssh_dir ]; then
+    $ssh_dir=/home/dvl/.ssh
+fi
 # rm -f $ssh_dir/id_ed25519 $ssh_dir/id_ed25519.pub 
 git config --global user.email $git_user_email 
 git config --global user.name $git_user_name
