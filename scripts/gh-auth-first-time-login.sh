1e33855bf7a5cd17c0039e57cbedff8cdf73f1d9
kindtek <n8@kindtek.io>
2023-03-19 15:16:22 -0700

    ssh auth

--- scripts/gh-auth-first-time-login.sh
+++ scripts/gh-auth-first-time-login.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git_user_email=$GH_REPO_OWNER_EMAIL
-git_user_name=kindtek
+git_user_name=kindtek@github.com
 ssh_dir=/home/$(echo ${1:-dvl})/.ssh
 if ! [ -d $ssh_dir ]; then
     $ssh_dir=/home/dvl/.ssh
@@ -10,7 +10,7 @@ fi
 git config --global user.email $git_user_email 
 git config --global user.name $git_user_name
 rm -f $ssh_dir/id_ed25519 $ssh_dir/id_ed25519.pub
-ssh-keygen -C $git_user_name@github.com -f $ssh_dir/id_ed25519 -N "" -t ed25519
+ssh-keygen -C $git_user_name -f $ssh_dir/id_ed25519 -N "" -t ed25519
 eval "$(ssh-agent -s)"
 ssh-add $ssh_dir/id_ed25519
 
