cc2a1b7fc9a746a1f74e1af8b68c367c34baa5e3
kindtek@github.com <n8@kindtek.io>
2023-03-22 11:50:42 -0700

    another fresh batch of kernels

--- kernel/build-zfs-kernel.sh
+++ kernel/build-zfs-kernel.sh
@@ -1,3 +1,4 @@
+_GBL=${_GBL:-gbl}
 sudo apt-get install -y snapd daemonize
 sudo daemonize /usr/bin/unshare --fork --pid --mount-proc /lib/systemd/systemd --system-unit=basic.target
 sleep 8
@@ -31,7 +32,8 @@ sudo sh copy-builtin ../wsl2 && \
 yes "" | sudo make install  && \
 cd ../wsl2/ && sudo sed -i 's/\# CONFIG_ZFS is not set/CONFIG_ZFS=y/g' .config && echo grep ZFS .config && \
 yes "" | sudo make -j16 && sudo make modules_install && \
-sudo mkdir -p ../../../gbl/kernels && sudo cp -f arch/x86/boot/bzImage ../../../gbl/kernels/amd/5_1519/linux-5_1519_za && \
+sudo mkdir -p ../../../${_GBL}/kernels && sudo cp -f arch/x86/boot/bzImage ../../../${_GBL}/kernels/amd/5_1519/linux-5_1519_za && \
+sudo mkdir -p ../../../dvl/kernels && sudo cp -f arch/x86/boot/bzImage ../../../dvl/kernels/amd/5_1519/linux-5_1519_zfs && \
 sudo cp arch/x86/boot/bzImage /mnt/c/users/$wsl_username/amd/5_1519/linux-5_1519_za
  
 # RUN sudo systemctl unmask snapd.service && sudo systemctl enable snapd.service && sudo systemctl start snapd.service && \
@@ -39,8 +41,8 @@ sudo cp arch/x86/boot/bzImage /mnt/c/users/$wsl_username/amd/5_1519/linux-5_1519
 
 #################################################################################
 
-
-
+_GBL=${_GBL:-gbl}
+cd /home/${_GBL}/dls && sudo rm -rf linux-6.2.7
 wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.2.7.tar.xz
 tar -xvf linux-6.2.7.tar.xz
 mv linux-6.2.7 linux
@@ -50,34 +52,28 @@ wsl_username=$(wslvar USERNAME)
 sudo cp /home/dvl/devels-work/devels-play/kernel/6_27/amd/.config_0 .config && \
 sudo chown $LOGNAME:$(id -gn) .config && \
 yes "" | sudo make prepare scripts && \
-# cd ../zfs && sudo sh autogen.sh && \
-# sudo sh configure --prefix=/ --libdir=/lib --includedir=/usr/include --datarootdir=/usr/share --enable-linux-builtin=yes --with-linux=/hal/dls/linux --with-linux-obj=/hal/dls/linux && \
-# sudo sh copy-builtin ../linux && \
-# yes "" | sudo make install  && \
-# cd ../linux/ && \
 sudo sed -i 's/\# CONFIG_ZFS is not set/CONFIG_ZFS=y/g' .config && echo grep ZFS .config && \
 yes "" | sudo make -j16 && sudo make modules_install && \
-sudo mkdir -p ../../../gbl/kernels/6_27_0/amd && sudo cp -f arch/x86/boot/bzImage ../../../gbl/kernels/6_27_0/amd/linux-6_27_0c && \
-sudo cp arch/x86/boot/bzImage /mnt/c/users/$wsl_username/linux-6_27_0c
+sudo mkdir -p ../../../${_GBL}/kernels/6_27/amd && sudo cp -f arch/x86/boot/bzImage ../../../${_GBL}/kernels/6_27_0/amd/linux-6_27_0e && \
+sudo mkdir -p ../../../dvl/kernels/6_27/amd && sudo cp -f arch/x86/boot/bzImage ../../../dvl/devels-work/devels-play/kernel/6_27/amd/linux-6_27_0 && \
+sudo cp -f arch/x86/boot/bzImage ../../../dvl/kernels/6_27/amd/linux-6_27_0e && \
+sudo cp arch/x86/boot/bzImage /mnt/c/users/$wsl_username/linux-6_27_0e
 
 
 
 ###############################################################################################
-cd /home/gbl/dls && rm -rf linux && \
+_GBL=${_GBL:-gbl}
+cd /home/${_GBL}/dls && sudo rm -rf linux linux-6.3-rc3 && \
 wget https://git.kernel.org/torvalds/t/linux-6.3-rc3.tar.gz && \
 tar -xvf linux-6.3-rc3.tar.gz && \
 mv linux-6.3-rc3 linux && \
 cd linux && \
 # wsl_username=$(wslvar USERNAME)
-sudo cp /hel/devels-work/devels-play/kernel/6_3-rc/.config_0 .config && \
+sudo cp /hel/devels-work/devels-play/kernel/6_3-rc/amd/.config_0 .config && \
 sudo chown $LOGNAME:$(id -gn) .config && \
 yes "" | sudo make prepare scripts && \
-# cd ../zfs && sudo sh autogen.sh && \
-# sudo sh configure --prefix=/ --libdir=/lib --includedir=/usr/include --datarootdir=/usr/share --enable-linux-builtin=yes --with-linux=/hal/dls/linux --with-linux-obj=/hal/dls/linux && \
-# sudo sh copy-builtin ../linux && \
-# yes "" | sudo make install  && \
-# cd ../linux/ && \
-# sudo sed -i 's/\# CONFIG_ZFS is not set/CONFIG_ZFS=y/g' .config && echo grep ZFS .config && \
 sudo make -j16 && sudo make modules_install && \
-sudo mkdir -p ../../../gbl/kernels/6_3-rc/amd && sudo cp -f arch/x86/boot/bzImage ../../../gbl/kernels/6_3-rc/amd/linux-6_3rc_a && \
-sudo cp arch/x86/boot/bzImage /mnt/c/users/$wsl_username/linux-6_3rc_a
+sudo mkdir -p ../../../${_GBL}/kernels/6_3-rc/amd && sudo cp -f arch/x86/boot/bzImage ../../../${_GBL}/kernels/6_3-rc/amd/linux-6_3rc_b && \
+sudo mkdir -p ../../../dvl/kernels/6_3-rc/amd && sudo cp -f arch/x86/boot/bzImage ../../../dvl/devels-work/devels-play/kernel/6_3-rc/amd/linux-6_3rc_0 && \
+sudo cp -f arch/x86/boot/bzImage ../../../dvl/kernels/6_3-rc/amd/linux-6_3rc_b && \
+sudo cp arch/x86/boot/bzImage /mnt/c/users/$wsl_username/linux-6_3rc_b
