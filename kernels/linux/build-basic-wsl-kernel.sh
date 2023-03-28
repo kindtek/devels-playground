e58a1425cfdf7bdaa4b1c722e17a7224af06b84e
kindtek <n8@kindtek.io>
2023-03-28 03:36:22 -0700

    reorg kernels

---
+++ kernels/linux/build-basic-wsl-kernel.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+cpu_vendor=$(grep -Pom 1 '^vendor_id\s*:\s*\K.*' /proc/cpuinfo)
+user_name=${2:-dvl}
+cpu_arch=$(uname -m)
+cpu_arch="${cpu_arch%%_*}"
+if ! [ -d /home/$user_name ]; then $user_name=/home/dvl; fi
+if [ $cpu_vendor = AuthenticAMD ]; then cpu_vendor=amd; fi
+if [ $cpu_vendor = GenuineIntel ]; then cpu_vendor=intel; fi
+
+linux_version_name="5.15.90.1"
+# replace first . with _ and then remove the rest of the .'s
+linux_version_mask=${linux_version_name/\./_}
+linux_version_mask=${linux_version_mask//[\.-]/}
+linux_mask=linux-$linux_version_mask
+
+save_name=linux-$linux_version_mask\_w0
+save_location1=$cpu_arch/$cpu_vendor/$linux_version_mask/$save_name
+save_location2=/home/$user_name/$save_name
+
+
+# try to pick the best .config file and default to the one provided by microsoft
+config_file_default=${1:-$cpu_arch/$cpu_vendor/$linux_version_mask/.config_wsl0}
+echo config_file:$config_file_default
+if ! [ -f $config_file_default ]; then config_file_default=$cpu_arch/generic/$linux_version_mask/.config_wsl0; fi
+echo config_file:config_file:$config_file_default
+
+if ! [ -f $config_file_default ]; then config_file_default=wsl2/Microsoft/config-wsl; fi
+echo config_file:$config_file_default
+
+if ! [ -f ${config_file} ]; then cp -fv $config_file_default wsl2/.config; config_file=$config_file_default; else cp -fv ${config_file} wsl2/.config; mkdir -p $cpu_arch/$cpu_vendor/$linux_version_mask; cp -bv $config_file $cpu_arch/$cpu_vendor/$linux_version_mask/.config_wsl0; fi
+echo config_file:$config_file_default
+
+printf '\n======= Kernel Build Info =========================================================================\n\n\tCPU Architecture:\t%s\n\n\tCPU Vendor:\t\t%s\n\n\tConfiguration File:\n\t\t%s\n\n\tSave Locations:\n\t\t%s\n\t\t%s\n\n===================================================================================================\n' $cpu_arch $cpu_vendor $config_file $save_location1 $save_location2
+
+
+# git clone https://github.com/microsoft/WSL2-Linux-Kernel.git wsl2 --progress --depth=1 --single-branch --branch linux-msft-wsl-5.15.90.1
+
+# cd wsl2
+
+# yes "" | make oldconfig && yes "" | make prepare
+# yes "" | make -j $(expr $(nproc) - 1)
+# make modules_install 
+# mkdir -pv /home/$user_name/kernels
+# mkdir -pv ../$cpu_arch/$cpu_vendor/$save_name
+# cp -fv --backup=numbered arch/x86/boot/bzImage /home/$user_name/$save_name
+# cp -fv --backup=numbered arch/x86/boot/bzImage /home/dvl/$save_name
+# cp -fv --backup=numbered arch/x86/boot/bzImage kernels/ubuntu/$cpu_arch/$cpu_vendor/$linux_version_mask/$save_name 
+# cp -fv --backup=numbered .config kernels/ubuntu/$cpu_arch/$cpu_vendor/$linux_version_mask/.config_wsl0
+
+# # rm -rf wsl2
