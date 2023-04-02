3e7b79cbfc5a4ac4c8a1894eb7d60984becbb493
kindtek <n8@kindtek.io>
2023-04-01 17:52:05 -0700

    cleanup cli script

--- docker/ubuntu/make-kernel-wsl-zfs-rc-amd.sh
+++ docker/ubuntu/make-kernel-wsl-zfs-rc-amd.sh
@@ -1,12 +1,20 @@
-sudo docker buildx build \
+timestamp=$(date +"%Y%m%d-%H%M%S")
+label=rc-wsl-zfs-kernel-builder
+filename="$label-$timestamp.log"
+user_name=$(wslvar USERNAME)
+command_string=sudo docker buildx build \
 --target dvlp_kernel-output \
---output type=local,dest=/mnt/c/users/n8kin/kindtek-wsl-zfs-linux-kernel-$(date +"%Y%m%d-%H%M%S").tar \
+--output type=local,dest=/mnt/c/users/${user_name:-default}/$filename.tar \
 --build-arg KERNEL_TYPE=latest-rc-wsl-zfs \
---build-arg REFRESH_REPO=yes" \
+--build-arg REFRESH_REPO=yes \
 --build-arg CONFIG_FILE= \
- . 2>&1 | \
-    sudo tee rc-wsl-zfs-kernel-builder"$(date)".log 
+ . 2>&1 | sudo tee $filename
+echo $command_string | sudo tee $filename
+echo $($command_string)
 
-echo 'sudo docker buildx build -t dvlp_kernel-output --output type=local,dest=/mnt/c/users/n8kin/kindtek-wsl-zfs-linux-kernel-$(date +"%Y%m%d-%H%M%S").tar 
-    --build-arg KERNEL_TYPE=latest-rc-wsl-zfs --build-arg REFRESH_REPO=yes --build-arg CONFIG_FILE= | sudo tee rc-wsl-zfs-kernel-builder"$(date)".log' \
-| sudo tee rc-wsl-zfs-kernel-builder"$(date)".log 
+ 
+# | sudo tee rc-wsl-zfs-kernel-builder"$timestamp".log 
+
+
+
+# sudo docker buildx build --target dvlp_kernel-output --output type=local,dest=/mnt/c/users/n8kin/kindtek-wsl-zfs-linux-kernel-$timestamp.tar --build-arg KERNEL_TYPE=latest-rc-wsl-zfs --build-arg REFRESH_REPO=true --build-arg CONFIG_FILE=  --no-cache .  2>&1 |     sudo tee rc-wsl-zfs-kernel-builder"$timestamp".log  
