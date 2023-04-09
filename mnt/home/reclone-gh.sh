609a09e2cc0a9b6499cdc2a2655ad6f013237e33
kindtek <98300737+kindtek@users.noreply.github.com>
2023-04-09 08:55:17 -0700

    meaner leaner and cleaner

--- mnt/home/reclone-gh.sh
+++ mnt/home/reclone-gh.sh
@@ -1,5 +1,5 @@
 apt-get install -y git
 rm -rf dvlw
-git clone https://github.com/kindtek/devels-workshop --filter=blob:limit=1024000 --depth=1 --single-branch --progress dvlw || (cd dvlw && git pull) 
+git clone https://github.com/kindtek/devels-workshop --single-branch --progress dvlw || (cd dvlw && git pull) 
 cd dvlw
-git submodule update --init --remote --depth=2 --progress || git pull
+git submodule update --init --progress || git pull
