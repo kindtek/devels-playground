45e8c44e04bcfd07bef7bad03b6234ab26331bea
Snail Mail <>
2023-04-12 05:52:46 -0700

    don't assume dir can be deleted

--- mnt/home/reclone-gh.sh
+++ mnt/home/reclone-gh.sh
@@ -1,5 +1,5 @@
 apt-get install -y git
-rm -rf dvlw
+rm -rf dvlw || echo 'couldn't remove dvlw directory ..'
 git clone https://github.com/kindtek/devels-workshop --single-branch --progress dvlw || (cd dvlw && git pull) 
 cd dvlw
 git submodule update --init --progress || git pull
