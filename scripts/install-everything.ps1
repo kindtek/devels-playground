7fb271577d2bb12882c8c2f5fa529096dc565c61
kindtek <98300737+kindtek@users.noreply.github.com>
2023-04-18 03:29:29 -0700

    better prompt

--- scripts/install-everything.ps1
+++ scripts/install-everything.ps1
@@ -208,10 +208,10 @@ function start_installer_daemon {
     
     Write-Host "$([char]27)[2J" 
     Write-Host "`r`nInstallations complete! Restart may be needed to complete begin WSL import phase. `r`n`r`n" 
+    reboot_prompt
     Start-Sleep 10
     if (!(require_docker_online)) {
         Write-Host "`r`nCannot start Docker.`r`n" 
-        reboot_prompt
     }
 }
 
