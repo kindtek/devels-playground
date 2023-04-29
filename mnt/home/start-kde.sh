06d4b160201eed03fa6d264b0660193ada04a5cb
kindtek@github.com <n8@kindtek.io>
2023-04-29 03:00:55 -0700

    copy over .rdp file to user windows home dir

--- mnt/home/start-kde.sh
+++ mnt/home/start-kde.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo apt-get update -y && sudo apt-get -y upgrade
 sudo /etc/init.d/xrdp start
+if [ ! -f ]
+    sudo cp /mnt/data/%HOME%/Kubuntu-GUI.rdp /mnt/c/
+fi
 port_num=${1:-3390}
 pwsh -Command /mnt/c/Windows/system32/mstsc.exe default.rdp /v:localhost:$port_num /admin /f /multimon || echo '
 oops. no gui
--- mnt/home/start-kex.sh
+++ mnt/home/start-kex.sh
@@ -1,2 +1,5 @@
+if [ ! -f ]
+    sudo cp /mnt/data/%HOME%/KEX-GUI.rdp /mnt/c/
+fi
 kex --win --start-client --sound
 # stop: kex --win --stop
