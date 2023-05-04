a4a970abb5f3250bb85f08522035bab8d9431e6b
kindtek@github.com <n8@kindtek.io>
2023-05-04 14:57:36 -0700

    username -> win_user; win_user instead of docker

--- docker/kali/make-kernel.sh
+++ docker/kali/make-kernel.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 timestamp=$(date +"%Y%m%d-%H%M%S")
 label=make-kernel
-username=$1
+win_user=$1
 kernel_type=$2
 kernel_feature=$3
 build_cache=${4:+'nocache'}
 filename="$label${kernel_type:+-$kernel_type}${kernel_feature:+-$kernel_feature}-$timestamp"
 
-while [ ! -d "/mnt/c/users/$username" ]; do
+while [ ! -d "/mnt/c/users/$win_user" ]; do
     echo " 
 
 
@@ -19,14 +19,14 @@ while [ ! -d "/mnt/c/users/$username" ]; do
     " 
     ls -da /mnt/c/users/*/ | tail -n +4 | sed -r -e 's/^\/mnt\/c\/users\/([ A-Za-z0-9]*)*\/+$/\t\1/g'
     read -r -p "
-" username
+" win_user
 done
 
 # docker_vols=$(docker volume ls -q)
 tee "$filename.sh" >/dev/null <<'TXT'
 #!/bin/bash
-username=${1}
-echo "username = $username"
+win_user=${1}
+echo "win_user = $win_user"
 kernel_type=${2:-basic}
 echo "kernel_type = $kernel_type"
 kernel_feature=${3}
@@ -40,10 +40,11 @@ docker_vols=$(docker volume ls -q)
 #
                     docker buildx build ${build_cache} \
                     --target dvlp_kernel-output \
-                    --output type=local,dest=/mnt/c/users/"${username}"/k-cache \
+                    --output type=local,dest=/mnt/c/users/"${win_user}"/k-cache \
                     --build-arg KERNEL_TYPE="${kernel_type}" \
                     --build-arg KERNEL_FEATURE="${kernel_feature}" \
                     --build-arg REFRESH_REPO=yes \
+                    --build-arg WIN_USER="${win_user}" \
                     --build-arg CONFIG_FILE= \
                     --progress=auto \
                     . 2>&1
@@ -55,6 +56,6 @@ TXT
 # copy the command to the log first
 eval cat "$filename.sh" 2>&1 | tee --append "$filename.log"
 # execute .sh file && log all output
-bash "${filename}.sh" "${username}" "${kernel_type}" "${kernel_feature}" "${build_cache}" | tee --append "${filename}.log"
+bash "${filename}.sh" "${win_user}" "${kernel_type}" "${kernel_feature}" "${build_cache}" | tee --append "${filename}.log"
 # prompt to install newly built kernel
-bash ../../kernels/linux/install-kernel.sh "$username" latest
+bash ../../kernels/linux/install-kernel.sh "$win_user" latest
