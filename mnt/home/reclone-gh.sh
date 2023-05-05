9cae678134f0af1bbf0328b2715c7a75aa5b4de6
kindtek@github.com <n8@kindtek.io>
2023-05-05 05:41:44 -0700

    better prompt

--- mnt/home/reclone-gh.sh
+++ mnt/home/reclone-gh.sh
@@ -1,11 +1,13 @@
+#!/bin/bash
 apt-get install -y git
 
 echo "  WARNING!
     Any changes you made in $HOME/dvlw are about to be erased.
 
-    continue    - press ENTER
-    skip        - type any key before pressing ENTER"
-read reclone
+    continue or exit?"
+read -r -p "
+(continue)
+" reclone
 if [ "$reclone" = "" ]; then
     echo 'rm -rf dvlw/* || echo "could not remove dvlw directory .."'
     echo 'rm -rf dvlw/.* || echo "could not remove git server .."'
@@ -17,11 +19,11 @@ repo_url=https://github.com/${repo_owner}/${repo_name} && \
 repo_path=dvlw && \
 repo_branch=main && \
 echo repo_owner: $repo_owner && \
-echo repo_name: $repo_name && \
-echo repo_url: $repo_url && \
-echo repo_path: $repo_path && \
-echo repo_branch: $repo_branch && \
-git clone "$repo_url" --single-branch --branch $repo_branch --filter=blob:limit=13k --progress -- $repo_path 2>/dev/nullcd dvlw && \
+echo repo_name: "$repo_name" && \
+echo repo_url: "$repo_url" && \
+echo repo_path: "$repo_path" && \
+echo repo_branch: "$repo_branch" && \
+git clone "$repo_url" --single-branch --branch "$repo_branch" --filter=blob:limit=13k --progress -- "$repo_path" 2>/dev/nullcd dvlw && \
 cd dvlw && \
 git submodule update --init --remote --filter=blob:limit=13k --progress -- dvlp dvl-adv powerhell 2>/dev/null && \
 chmod -R 1770 dvlp/mnt/home && \
