4bbfe049fb8baa87e4ad9ac5990c3841afa719aa
kindtek@github.com <n8@kindtek.io>
2023-05-09 21:46:48 -0700

    add info message

--- docker/kali/make-kernel.sh
+++ docker/kali/make-kernel.sh
@@ -8,6 +8,10 @@ config_file=${4}
 filename="$label${kernel_type:+-$kernel_type}${kernel_feature:+-$kernel_feature}-$timestamp"
 
 while [ "$win_user" = "" ] || [ ! -d "/mnt/c/users/$win_user" ]; do
+    if [ "$win_user" != "" ]; then
+        echo "could not find C:\\users\\$win_user"
+    fi
+
     echo "
 
 
