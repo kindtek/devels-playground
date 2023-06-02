ce06f53f2b01fd91db5c7a95c460112517ee7d59
kindtek@github.com <n8@kindtek.io>
2023-06-01 20:53:07 -0700

    eliminate white space

--- scripts/install-everything.ps1
+++ scripts/install-everything.ps1
@@ -291,7 +291,7 @@ function start_installer_daemon {
     wsl --update --pre-release
     [Console]::OutputEncoding = [System.Text.Encoding]::Unicode
     $default_wsl_distro = wsl --list | Where-Object { $_ -and $_ -ne '' -and $_ -match '(.*)\(Default\)' }
-    $default_wsl_distro = $default_wsl_distro -replace '^(.*)(\(Default\))$', '$1'
+    $default_wsl_distro = $default_wsl_distro -replace '^(.*)(\s\(Default\))$', '$1'
     Write-Host "temporarily switching default WSL distro from `"$default_wsl_distro`" to Ubuntu"
     wsl -s Ubuntu
     # Write-Host "$([char]27)[2J" 
