450021239c73de62a00223b2101e4e57688c22cc
kindtek <98300737+kindtek@users.noreply.github.com>
2023-06-03 08:18:16 -0700

    set json output encoding as utf-8

--- scripts/install-everything.ps1
+++ scripts/install-everything.ps1
@@ -138,7 +138,7 @@ function set_docker_config {
         $config_json.integratedWslDistros = $jcurrent + $jnew
     }
 
-    ConvertTo-JSON $config_json -Depth 2 | Out-File $config_file -Force
+    ConvertTo-JSON $config_json -Depth 2 | Set-Content -Encoding utf8 | Out-File $config_file -Force
 
 }
 
