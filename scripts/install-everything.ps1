b6bc504a9534cec86254e9e05cda46f7cd65efd8
kindtek <98300737+kindtek@users.noreply.github.com>
2023-06-28 09:45:52 -0700

    possible wsl output encoding fix

--- scripts/install-everything.ps1
+++ scripts/install-everything.ps1
@@ -1,5 +1,6 @@
 $host.UI.RawUI.ForegroundColor = "White"
 $host.UI.RawUI.BackgroundColor = "Black"
+$env:WSL_UTF8=1
 # source of the below self-elevating script: https://blog.expta.com/2017/03/how-to-self-elevate-powershell-script.html#:~:text=If%20User%20Account%20Control%20(UAC,select%20%22Run%20with%20PowerShell%22.
 # Self-elevate the script if required
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
