938eeaeab83554515cf0163962e6032b8353bf0f
kindtek <98300737+kindtek@users.noreply.github.com>
2023-07-15 20:10:43 -0700

    no more debug

--- scripts/wsl-docker-import.cmd
+++ scripts/wsl-docker-import.cmd
@@ -5,7 +5,7 @@ SETLOCAL EnableDelayedExpansion
 
 doskey /exename docker.exe docker=C:\Program Files\Docker\Docker\Docker Desktop.exe > nul 2> nul
 doskey /exename wsl.exe wsl=C:\Windows\System32\wsl.exe > nul 2> nul
-SET "DVLP_DEBUG=y"
+SET "DVLP_DEBUG=n"
 :redo
 SET "module=main"
 SET wsl_version_int=2
