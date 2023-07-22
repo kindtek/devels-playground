ca3eded6d617eae3eba9d07a9e7a75a757a7819a
kindtek <98300737+kindtek@users.noreply.github.com>
2023-07-21 17:45:14 -0700

    kernel maker

--- scripts/wsl-docker-import.cmd
+++ scripts/wsl-docker-import.cmd
@@ -218,8 +218,8 @@ IF "!image_service_suffix!"=="kernel" (
     @REM TODO: add prompt (when noninteractive) for kernel type/feature
     SET "build_args=--build-arg WIN_USER=%USERNAME%"
     SET "build_args=!build_args! --build-arg KERNEL_TYPE=basic --build-arg KERNEL_FEATURE=zfs"
-    SET "compose_services_nocache=!compose_services_nocache! repo-kernel kernel-maker"
-    SET "compose_services=kernel-make !image_service!"
+    SET "compose_services_nocache=!compose_services_nocache! repo-kernel"
+    SET "compose_services=kernel-maker !image_service!"
     @REM SET "compose_services=kernel-make !image_service_base! !image_service!"
 
     @REM SET "build_args= !build_args! --build-arg KERNEL_FEATURE='zfs'"
