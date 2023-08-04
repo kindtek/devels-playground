460985e9c67cd3c455d83531af410e1e83b10378
kindtek <98300737+kindtek@users.noreply.github.com>
2023-08-04 03:11:27 -0700

    kernel-make order

--- scripts/wsl-docker-import.cmd
+++ scripts/wsl-docker-import.cmd
@@ -242,7 +242,7 @@ IF "!image_service_suffix!"=="kernel" (
     SET "build_args=--build-arg WIN_USER=%USERNAME%"
     SET "build_args=!build_args! --build-arg KERNEL_TYPE=stable --build-arg KERNEL_FEATURE=zfs"
     SET "compose_services_nocache=!compose_services_nocache! repo-kernel"
-    SET "compose_services=!image_service! kernel-make"
+    SET "compose_services=kernel-make !image_service!"
     @REM SET "compose_services=kernel-maker !image_service_base! !image_service!"
 )
 IF "!image_service!" NEQ "test" (
