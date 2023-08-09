c6225e1e210febb3d8b4258ada0af6674f2aff8e
kindtek <98300737+kindtek@users.noreply.github.com>
2023-08-09 10:47:16 -0700

    try out using full timestamp (both date & time)

--- scripts/wsl-docker-import.cmd
+++ scripts/wsl-docker-import.cmd
@@ -288,12 +288,12 @@ IF "!wsl_distro!" == "!failsafe_wsl_distro!" (
     @REM IF "!wsl_out!" == "!test_string!" (
     @REM     @REM we have a duplicate
     SET "wsl_distro_base=!wsl_distro!"
-    SET "wsl_distro=!wsl_distro!-!timestamp_time!"
+    SET "wsl_distro=!wsl_distro!-!timestamp_date!!timestamp_time!"
     SET "save_location=!mount_drive!:\!save_directory!\!wsl_distro_base!"
-    SET "install_root_dir=!save_location!\!timestamp_time!"
-    SET "image_save_path=!save_location!\!timestamp_time!\backups\!wsl_distro!.tar"
+    SET "install_root_dir=!save_location!\!timestamp_date!!timestamp_time!"
+    SET "image_save_path=!save_location!\!timestamp_date!!timestamp_time!\backups\!wsl_distro!.tar"
     SET "install_location=!install_root_dir!"
-    mkdir !save_location!\!timestamp_time!\backups > nul 2> nul
+    mkdir !save_location!\!timestamp_date!!timestamp_time!\backups > nul 2> nul
     mkdir !install_location! > nul 2> nul
     IF "!DVLP_DEBUG!" == "y" (
         ECHO duplicate distro found for !wsl_distro!
