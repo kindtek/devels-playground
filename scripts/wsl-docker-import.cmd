78c786b411b7d205f76bbfca2f72afe38bb726ef
kindtek <98300737+kindtek@users.noreply.github.com>
2023-08-09 12:23:57 -0700

    kill dbug

--- scripts/wsl-docker-import.cmd
+++ scripts/wsl-docker-import.cmd
@@ -5,7 +5,7 @@ SETLOCAL EnableDelayedExpansion
 
 doskey /exename docker.exe docker=C:\Program Files\Docker\Docker\Docker Desktop.exe > nul 2> nul
 doskey /exename wsl.exe wsl=C:\Windows\System32\wsl.exe > nul 2> nul
-SET "DVLP_DEBUG=y"
+SET "DVLP_DEBUG=n"
 :redo
 IF "!DVLP_DEBUG!" == "y" (
     ECHO "custom image set with !image_name_tag!"
